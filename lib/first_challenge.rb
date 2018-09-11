0ef2393929e251dc31813204e12711a5835b9788
Rajvir Rai <rsr1992@gmail.com>
2018-09-11 00:32:26 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 contacts ["Freddy Mercury"].each do  | attribute, value |
   if attribute == :favorite_icecream_flavors
-    value.delete_if 
+    value.delete_if do |flavor| 
   #your code here
 
 
