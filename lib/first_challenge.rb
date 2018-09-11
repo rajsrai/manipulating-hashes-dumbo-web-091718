3426342be14207e3aab8a959fd4c16746cb891b2
Rajvir Rai <rsr1992@gmail.com>
2018-09-11 00:30:35 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-contacts.delete_if {|:favorite_icecream_flavors, 0| key >= "cookie dough" }
+contacts {|:favorite_icecream_flavors, 0| key >= "cookie dough" }
   #your code here
 
 
